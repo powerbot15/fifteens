8ece4f0a439d277d2db5e008b394f7e54fdc54f3
powerbot15 <powerbot15@gmail.com>
2014-02-18 23:29:04 +0200

    some draggable

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 jQuery(function($){
 
-
+    $('.game-field td').droppable();
+    $('.game-field td>div').draggable().disableSelection();
+    $('.game-field td').droppable();
+//    $('.game-field td').sortable().disableSelection();
 
 });

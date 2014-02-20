7fa5b2ddac6197bd625916372df02fac1b2dfcf6
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2014-02-20 13:47:27 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,80 +1,59 @@
+jQuery(function prepareGameField($){
 
-jQuery(function($){
-
-    (function prepareGameField(){
-
-        var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-            gameField = $('.game-field td>div').draggable({zIndex: 100});
-
-        values = values.shuffle();
-
-        for( var i = 0; i < gameField.length; i++){
-            gameField.eq(i).text(values[i]);
-        }
-
-        var stepCount = 0;
-
-        $('body').droppable({
-            drop: function(e, ui){
-                ui.draggable.css('top', '0px');
-                ui.draggable.css('left', '0px');
-            }
-        }).disableSelection();
-
-        $('.game-field td').droppable({
-            drop: function (e, ui) {
-                var numberCell = $(this),
-                    distanceX = Math.abs(ui.position.left),
-                    distanceY = Math.abs(ui.position.top);
-
-                if ((numberCell.children().length > 0) ||
-                    (distanceX > numberCell.width() * 0.5) && (distanceY > numberCell.height() * 0.5) ||
-                    (distanceX > numberCell.width() * 1.5) ||
-                    (distanceY > numberCell.height() * 1.5)){
-                    magnetTopLeft(ui);
-                    return;
-                }
-
-                magnetTopLeft(ui);
-                ui.draggable.appendTo(this);
-                stepCount++;
-                checkWin(gameField);
-            }
+    var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].shuffle(),
+        cells = $('.cell').draggable({
+            helper: 'clone',
+            appendTo: 'body',
+            zIndex: 100
         });
+        
+    cells.text(function(i){
+        return values[i];
+    });
 
-        function checkWin(currentState){
+    var stepCount = 0;
 
-            for (var i = 0; i < currentState.length; i++){
-                if (currentState.eq(i).text() != i + 1){return}
-            }
+    $('.game-field td').droppable({
+        drop: function (e, ui) {
+            var cell = $(this);
 
-            $('.win-inform').text('Yeah, You did it !!! You collected it in ' + stepCount + 'steps');
+            if (cell.children().length > 0) return;
 
-            currentState.draggable({disabled: true});
-
+            ui.draggable.appendTo(this);
+            stepCount++;
+            checkWin(gameField);
         }
+    });
 
-    })();
+    function checkWin(){
+        var win = false;
+        $('.cell').each(function(i){
+            return win = this.innerHTML == i + 1;
+        });
+        
+        if (! win) return;
 
-});
-//region =================Utils======================
+        $('.win-inform').text('Yeah, You did it !!! You collected it in ' + stepCount + 'steps');
+
+        currentState.draggable({disabled: true});
+
+    }
+
+    //region =================Utils======================
 
 
-function magnetTopLeft(ui){
-    ui.draggable.css('top', '0px');
-    ui.draggable.css('left', '0px');
-}
-
-
-(function setShuffleMethod(){
+    function magnetTopLeft(ui){
+        ui.draggable.css('top', '0px');
+        ui.draggable.css('left', '0px');
+    }
+    
+    
     Array.prototype.shuffle = function(){
-        var
-            randomIndex,
+        var randomIndex,
             exchangeVar,
             countOfElements = this.length;
 
         for(var i = 0; i < countOfElements; i++){
-
             randomIndex = getRandomInt(0, countOfElements - 1);
 
             exchangeVar = this[randomIndex];
@@ -84,16 +63,14 @@ function magnetTopLeft(ui){
         }
 
         return this;
-
     };
-})();
+    
+    
+    function getRandomInt(min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    
+    //endregion
 
-
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-
-
-//endregion
+});

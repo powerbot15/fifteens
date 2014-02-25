47f49b7cc6e1d23350e9cd0dba3b42af68b3d2d1
powerbot15 <powerbot15@gmail.com>
2014-02-25 21:28:43 +0200

    fixed bug checking win information optimization and refactoring

--- js/main.js
+++ js/main.js
@@ -1,59 +1,64 @@
 
-jQuery( function prepareGameField($){
-
-        var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].shuffle(),
-            $numberGameElement = $('.number').draggable({zIndex: 100});
-
-        for( var i = 0; i < $numberGameElement.length; i++){
-            $numberGameElement.eq(i).text(values[i]);
-        }
-
-        var stepCount = 0;
-
-        $('body').droppable({
-            drop: function(e, ui){
-                magnetTopLeft(ui);
-            }
-        }).disableSelection();
-
-        $('.game-field-cell').droppable({
-            drop: function (e, ui) {
-                var $numberCell = $(this),
-                    distanceX = Math.abs(ui.position.left),
-                    distanceY = Math.abs(ui.position.top);
-
-                if (($numberCell.children().length > 0) ||
-                    (distanceX > $numberCell.width() * 0.5) && (distanceY > $numberCell.height() * 0.5) ||
-                    (distanceX > $numberCell.width() * 1.5) ||
-                    (distanceY > $numberCell.height() * 1.5)){
-                    magnetTopLeft(ui);
-                    return;
-                }
-
-                magnetTopLeft(ui);
-                ui.draggable.appendTo(this);
-                stepCount++;
-                checkWin($numberGameElement);
-            }
-        });
-
-        function checkWin(currentState){
-
-            for (var i = 0; i < currentState.length; i++){
-                if (currentState.eq(i).text() != i + 1){return}
-            }
-
-            $('.win-inform').text('Yeah, You did it !!! You collected it in ' + stepCount + 'steps');
-
-            currentState.draggable({disabled: true});
-
-        }
-
-
-
+jQuery(function($){
+    prepareGameField($);
+    $('.refresh-button').on('click', function(){
+        $('.win-inform').text('');
+        prepareGameField($)
+    });
 });
+
 //region =================Utils======================
 
+function prepareGameField($){
+
+    var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].shuffle(),
+        $numberGameElements = $('.number').draggable({zIndex: 100});
+    $numberGameElements.draggable({disabled: false});
+    for( var i = 0; i < $numberGameElements.length; i++){
+        $numberGameElements.eq(i).text(values[i]);
+    }
+
+    var stepCount = 0;
+
+    $('body').droppable({
+        drop: function(e, ui){
+            magnetTopLeft(ui);
+        }
+    }).disableSelection();
+
+    $('.game-field-cell').droppable({
+        drop: function (e, ui) {
+            var $numberCell = $(this),
+                distanceX = Math.abs(ui.position.left),
+                distanceY = Math.abs(ui.position.top);
+
+            if (($numberCell.children().length > 0) ||
+                (distanceX > $numberCell.width() * 0.5) && (distanceY > $numberCell.height() * 0.5) ||
+                (distanceX > $numberCell.width() * 1.5) ||
+                (distanceY > $numberCell.height() * 1.5)){
+                magnetTopLeft(ui);
+                return;
+            }
+
+            magnetTopLeft(ui);
+            ui.draggable.appendTo(this);
+            stepCount++;
+            checkWin($('.number')); //WTF jQuery objects aren't update themselves automatically
+        }
+    });
+
+    function checkWin(currentState){
+
+        for (var i = 0; i < currentState.length; i++){
+            if (currentState.eq(i).text() != i + 1){return}
+        }
+
+        $('.win-inform').text('Yeah, You did it !!! You collected it in ' + stepCount + 'steps');
+
+        currentState.draggable({disabled: true});
+
+    }
+}
 
 function magnetTopLeft(ui){
     ui.draggable.css('top', '0px');
